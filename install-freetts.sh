e07e900db359dcbd51d9a23777cc61d5b9ee022d
disik <disik69@meta.ua>
2024-10-20 12:49:42 +0300

    master: create maven base, spring boot base, freetts base

---
+++ install-freetts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $1 = "" ]]; then
+    echo "The script needs a maven binary file for installing local freetts libraries to repository."
+    echo "The format is \"$0 <path to binary file>\""
+    exit 0
+fi
+
+$1 install:install-file -Dfile=freetts/cmu_us_kal.jar -DgroupId=ua.pp.disik -DartifactId=cmu_us_kal -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+$1 install:install-file -Dfile=freetts/cmulex.jar -DgroupId=ua.pp.disik -DartifactId=cmulex -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+$1 install:install-file -Dfile=freetts/en_us.jar -DgroupId=ua.pp.disik -DartifactId=en_us -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+$1 install:install-file -Dfile=freetts/freetts.jar -DgroupId=ua.pp.disik -DartifactId=freetts -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
---
+++ src/main/java/ua/pp/disik/englishroulette/desktop/Application.java
@@ -0,0 +1,11 @@
+package ua.pp.disik.englishroulette.desktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
