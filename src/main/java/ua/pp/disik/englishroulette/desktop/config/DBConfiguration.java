604c1f59866f58a5cd9fa205cc9e815473058df5
disik <disik69@meta.ua>
2025-03-16 12:45:59 +0200

    fx: refactor

--- src/main/java/ua/pp/disik/englishroulette/desktop/config/DBConfiguration.java
+++ src/main/java/ua/pp/disik/englishroulette/desktop/config/DBConfiguration.java
@@ -3,7 +3,6 @@ package ua.pp.disik.englishroulette.desktop.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -12,17 +11,15 @@ import javax.sql.DataSource;
 @Configuration
 @PropertySource("persistence.properties")
 public class DBConfiguration {
-    private final Environment env;
-
-    public DBConfiguration(Environment env) {
-        this.env = env;
-    }
-
     @Bean
-    public DataSource dataSource(@Value("${dbFilename:}") String urlFileName) {
+    public DataSource dataSource(
+            @Value("${db.driverClassName:}") String driverClassName,
+            @Value("${db.urlPrefix:}") String urlPrefix,
+            @Value("${db.filename:}") String urlFilename
+    ) {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("driverClassName"));
-        dataSource.setUrl(env.getProperty("urlPrefix") + urlFileName);
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(urlPrefix + urlFilename);
         return dataSource;
     }
 }
